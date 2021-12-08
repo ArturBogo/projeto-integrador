7b98cac73c5274112b4ce57fb0c1fa67c9d30fa8
Artur Bogo <50053320+ArturBogo@users.noreply.github.com>
2021-12-07 23:28:19 -0300

    PRojeto 1

---
+++ Projeto/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDUJWRYwMzejwaQ6b4KDqFiA0E2RzfhnDY",
+  authDomain: "projeto-integrador-600a0.firebaseapp.com",
+  databaseURL: "https://projeto-integrador-600a0.firebaseio.com",
+  projectId: "projeto-integrador-600a0",
+  storageBucket: "projeto-integrador-600a0.appspot.com",
+  messagingSenderId: "1036140096889",
+  appId: "1:1036140096889:web:a56105a43a256452df13a8",
+};
+
+firebase.initializeApp(firebaseConfig);
---
+++ Projeto/script.js
@@ -0,0 +1,50 @@
+const db = firebase.firestore();
+let tasks = [];
+let currentUser = {};
+
+
+
+function getUser() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      currentUser.uid = user.uid;
+      let userLabel = document.getElementById("usuarioLogin");
+      userLabel.innerHTML = user.email;
+    } else {
+      swal
+        .fire({
+          icon: "success",
+          title: "Redirecionando para a tela de autenticação",
+        })
+        .then(() => {
+          setTimeout(() => {
+            window.location.replace("login.html");
+          }, 1000);
+        });
+    }
+  });
+}
+
+var slideIndex = 0;
+Slides();
+
+function Slides() {
+  var i;
+  var slides = document.getElementsByClassName("carrossel");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(Slides, 2500);
+}
+
+window.onload = function () {
+  getUser();
+};
